7182bbf0a83ebcc93fab75f5205e08a173e7fcf4
Christian Manalansan <cmanalansan@gmail.com>
2021-09-14 23:01:40 -0400

    Handle query parameters

--- src/Controller/MemoryApiController.php
+++ src/Controller/MemoryApiController.php
@@ -2,19 +2,23 @@
 
 namespace Drupal\memory_game_setup\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Memory HTTP API Controller Class
  */
 class MemoryApiController {
-  public function render() {
+  public function render(Request $request) {
+    $rows = $request->query->get('rows');
+    $columns = $request->query->get('columns');
+
     return new JsonResponse([
       'meta' => [
         'success' => TRUE,
-        'cardCount' => 4,
+        'cardCount' => $rows * $columns,
         'uniqueCardCount' => 2,
-        'uniqueCards' => ['D', 'G']
+        'uniqueCards' => ['D', 'G'],
       ],
       'data' => [
         'cards' => [
