1c4b2411c81e8249ba5b7fc962f72b5cc164b356
Christian Manalansan <cmanalansan@gmail.com>
2021-09-14 23:13:56 -0400

    Handle missing parameters

--- src/Controller/MemoryApiController.php
+++ src/Controller/MemoryApiController.php
@@ -10,7 +10,11 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 class MemoryApiController {
   public function render(Request $request) {
+    // sanitize and validate parameters
     $rows = $request->query->get('rows');
+    if ($rows < 1 || $rows > 6) {
+      return $this::get_error_response();
+    }
     $columns = $request->query->get('columns');
 
     return new JsonResponse([
@@ -26,6 +30,16 @@ class MemoryApiController {
           ['G', 'D'],
         ]
       ],
+    ]);
+  }
+
+  static function get_error_response() {
+    return new JsonResponse([
+      'meta' => [
+        'success' => FALSE,
+        'message' => 'Either "rows" or "columns" needs to be an even number.'
+      ],
+      'data' => [],
     ]);
   }
 }
