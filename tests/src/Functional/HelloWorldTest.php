7a7cbb191ccd35bd4832be948d051182e9702bd6
Christian Manalansan <cmanalansan@gmail.com>
2021-09-14 22:48:23 -0400

    Create failing test: response should have 'meta' and 'data'

--- tests/src/Functional/HelloWorldTest.php
+++ tests/src/Functional/HelloWorldTest.php
@@ -5,6 +5,10 @@ namespace Drupal\memory_game_setup\Tests;
 use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Test case for API request
+ * @see https://symfonycasts.com/screencast/rest/testing-phpunit
+ */
 class HelloWorldTest extends TestCase {
   public function testGET() {
     // create our http client (Guzzle)
@@ -12,8 +16,13 @@ class HelloWorldTest extends TestCase {
       'base_uri' => 'http://localhost:8000',
     ]);
 
+    // does this page exist?
     $response = $client->get('/code-challenge/card-grid?rows=2&columns=2');
-
     $this->assertEquals(200, $response->getStatusCode());
-  }  
+    
+    // The endpoint takes 2 query parameters: rows and columns.
+    $json = json_decode($response->getBody(TRUE), TRUE);
+    $this->assertArrayHasKey('data', $json);
+    $this->assertArrayHasKey('meta', $json);
+  }
 }
